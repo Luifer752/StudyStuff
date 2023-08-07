ab6ddbc924c41576f48be46cd83deb03ea4132f2
Finn Jakobs <turutututututu@gmail.com>
2023-08-07 13:13:38 -0500

    ContextMan HW

--- Homework/HW.py
+++ Homework/HW.py
@@ -1,32 +1,40 @@
-from time import time
-from functools import wraps
+from  contextlib import contextmanager
+from time import time, sleep
 
+@contextmanager
+def context_manager():
+    start = time()
 
-def rate_limit(num=3, time_lim=10):
-    call_time = []
-    def dec_1(func):
-        call_time1 = [i for i in call_time if i <= time_lim]
-        @wraps(func)
-        def inner(*args, **kwargs):
+    yield
+    sleep(1)
+    end = time()
+    print(end - start)
 
-            start = time()
-            counter = 0
+with context_manager() as manager:
+    print("Printing smth for hte first time")
 
-            for i in range(num):
-                func(*args, **kwargs)
-                if len(call_time1) > time_lim:
-                    print("Call limit reached")
-                    break
-                else:
-                    call_time1.append(start)
+class ContextManager:
+    runtime = []
 
-        return inner
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        start = time()
+        ContextManager.runtime.append(start)
+        print("Init called")
 
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
 
-    return dec_1
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end = time()
+        ContextManager.runtime.append(end)
+        self.file.close()
+        print(f"Runtime is: {ContextManager.runtime[1] - ContextManager.runtime[0]}")
 
-@rate_limit(7, 2)
-def say_hi(name):
-    print(f"Hello, {name}")
+with ContextManager('example.txt', 'w') as m:
+    m.write("New Line")
+    print("Printing smth AGAIN")
 
-say_hi("sd")
+print(m.closed)
