44eab6122ed1396089a826791a022e0a3f759f3b
Finn Jakobs <turutututututu@gmail.com>
2023-08-07 11:40:50 -0500

    Rate limit V2.1

--- Homework/HW.py
+++ Homework/HW.py
@@ -3,27 +3,29 @@ from functools import wraps
 
 
 def rate_limit(num=3, time_lim=10):
-
+    call_time = []
     def dec_1(func):
-        call_time = []
+        call_time1 = [i for i in call_time if i <= time_lim]
         @wraps(func)
         def inner(*args, **kwargs):
+
             start = time()
             counter = 0
 
             for i in range(num):
                 func(*args, **kwargs)
-                counter += 1
-                a.append(start)
-                if start == time_lim or counter == num:
+                if len(call_time1) > time_lim:
+                    print("Call limit reached")
                     break
+                else:
+                    call_time1.append(start)
 
         return inner
 
 
     return dec_1
 
-@rate_limit(4, 2)
+@rate_limit(7, 2)
 def say_hi(name):
     print(f"Hello, {name}")
 
