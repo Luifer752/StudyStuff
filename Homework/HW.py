a9b1a6989417d41a5a7fbdba5ec09f7cae9b64aa
Finn Jakobs <turutututututu@gmail.com>
2023-08-07 14:31:54 -0500

    Gen FiZZ BuZZ

--- Homework/HW.py
+++ Homework/HW.py
@@ -1,39 +1,26 @@
-from  contextlib import contextmanager
-from time import time, sleep
+inp = 17
 
-@contextmanager
-def context_manager(sym):
-    print(10 * f"{sym}")
 
-    yield
+def even_gen(inp):
+    counter = 0
+    while counter < inp:
+        if counter % 3 == 0 and counter % 5 == 0:
+            yield "FizzBuzz"
+            counter += 1
+            continue
+        elif counter % 3 == 0:
+            yield "Fizz"
+            counter += 1
+            continue
+        elif counter % 5 == 0:
+            yield "Buzz"
+            counter += 1
+            continue
+        else:
+            counter += 1
+            yield counter
 
-    print(10 * f"{sym}")
+even = even_gen(inp)
 
-with context_manager("-") as manager:
-    print("Printing smth for hte first time")
-
-class ContextManager:
-    runtime = []
-
-    def __init__(self, filename, mode, sym):
-        self.filename = filename
-        self.mode = mode
-        self.sym = sym
-
-        print(10 * f"{self.sym}")
-
-    def __enter__(self):
-        self.file = open(self.filename, self.mode)
-        return self.file
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        end = time()
-        ContextManager.runtime.append(end)
-        self.file.close()
-        print(10 * f"{self.sym}")
-
-with ContextManager('example.txt', 'w', "*") as m:
-    m.write("New Line")
-    print("Printing smth AGAIN")
-
-print(m.closed)
+for i in even:
+    print(i)
