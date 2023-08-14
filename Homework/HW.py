ab4b6fbc8445de0f1091580b61cb3e845c275bf1
Finn Jakobs <turutututututu@gmail.com>
2023-08-14 08:22:02 -0500

    Private Attr

--- Homework/HW.py
+++ Homework/HW.py
@@ -1,12 +1,25 @@
-def fibonacci_generator():
-    a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
+'''Напишіть клас "Person", який має властивості name (ім'я) і age (вік). ' \
+Зробіть ці властивості приватними, щоб вони не могли бути змінені напряму ззовні класу. ' \
+Забезпечте методи для доступу до цих властивостей та встановлення їх значень.'''
 
 
-fibonacci_gen = fibonacci_generator()
+class Char:
 
-for i in range(100):
-    print(next(fibonacci_gen))
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
+    def nick_change(self, new):
+        self.__name = new
+
+    def youth_potion(self, years):
+        self.__age = years
+
+    def print_char(self):
+        print(f"{self.__name}, {self.__age}")
+
+ch1 = Char("Gilbert", 42)
+ch2 = Char("Fred", 12)
+ch1.nick_change("Vasya")
+
+ch1.print_char()
